09e39de50cb83215a1446c692d1e75e5188da8c3
meta-vinay-pancholi <vinay.pancholi@metacube.com>
2019-07-22 17:06:43 +0530

    PF/ASSIGNMENT11 VARIOUS OPERATION ON SETS

---
+++ PF/ASSIGNMENT11/src/AllTests.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ getComplementTestCase.class, getUnionTestCase.class,
+		IntSetTestCases.class, isMemberTestCase.class, isSubSetTestCase.class })
+public class AllTests {
+
+}
