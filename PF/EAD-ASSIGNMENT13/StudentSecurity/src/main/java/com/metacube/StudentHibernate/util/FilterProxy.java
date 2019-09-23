43780be07cdd39b5e227d8d887895ff0630aa7b1
meta-vinay-pancholi <vinay.pancholi@metacube.com>
2019-09-23 17:46:25 +0530

    EAD - SESSION 13 SPRING SECURITY

---
+++ PF/EAD-ASSIGNMENT13/StudentSecurity/src/main/java/com/metacube/StudentHibernate/util/FilterProxy.java
@@ -0,0 +1,12 @@
+package com.metacube.StudentHibernate.util;
+
+import org.springframework.web.filter.DelegatingFilterProxy;
+
+/**
+ * extending delegatingFilterproxy class
+ * @author vinay
+ *
+ */
+public class FilterProxy extends DelegatingFilterProxy {
+
+}
